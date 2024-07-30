e466e4e2df001a6488cb64b88b884cae6c08672a
Gabriel <gabrielm03@alu.ufc.br>
2024-07-30 20:53:45 -0300

    Adding the connected disconneted idea

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
-const express = require("express")
-const {Server} = require("socket.io")
-const { createServer } = require('node:http')
-const path = require('path')
+const express = require("express");
+const { Server } = require("socket.io");
+const { createServer } = require('node:http');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 //This is necessary to give direct acess of http server to socket.io
-const server = createServer(app)
+const server = createServer(app);
+const io = new Server(server);
 
-const io = new Server(server)
 
-app.get("/", (req,res)=>{
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
-})
+});
 
-app.listen(3000, ()=>{
-    console.log("Server is running!")
-})
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
