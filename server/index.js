eca51383606aabfede7c43a5678603c85d24828b
Gabriel <gabrielm03@alu.ufc.br>
2024-07-30 19:38:48 -0300

    Creating the http server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const {Server} = require("socket.io")
+const { createServer } = require('node:http')
 
 const app = express()
+
+//This is necessary to give direct acess of http server to socket.io
+const server = createServer(app)
 
 app.get("/", (req,res)=>{
     res.send("<h1>Server is on!</h1>")
