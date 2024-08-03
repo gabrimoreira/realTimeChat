c30193b0d946f96d536d7fbdf1d929f4f687fc35
Gabriel <gabrielm03@alu.ufc.br>
2024-08-03 17:20:15 -0300

    Chat with rooms add

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const { Server } = require("socket.io");
 const { createServer } = require('node:http');
 const path = require('path');
 
+
 const app = express();
 
 //This is necessary to give direct acess of http server to socket.io
@@ -16,18 +17,30 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
+app.get("/room", (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/room.html'));
+});
+
 io.on('connection', (socket) => {
     console.log('A user connected');
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
-    socket.on('message', (msg)=>{
+    socket.on('joinRoom', ({ id, room }) => {
+        console.log(`User with ID ${id} joined room ${room}`);
+        socket.join(room);
+    });
+
+    socket.on('message', ({room, msg})=>{
         console.log("Message: " + msg);
-        socket.broadcast.emit('message', msg)
+        console.log("Room: " + room);
+        socket.to(room).emit('message', msg)
     } 
 )
 
 });
+
+
 
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
