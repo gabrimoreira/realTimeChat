12e4867939663b60213b9340fc6fc7a51d2458dc
Igor Pereira Gouveia <ipgouveia@hotmail.com>
2024-08-14 09:28:32 -0300

    Customization's first version

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -35,22 +35,34 @@ const Room = () => {
     };
 
     return (
-        <div>
-            <ul>
-                {messages.map((msg, index) => (
-                    <li key={index}>{msg}</li>
-                ))}
-            </ul>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    required
-                />
-                <button type="submit">Send</button>
-            </form>
+        <div class="bg-black flex flex-col items-center justify-center w-screen min-h-screen p-10">
+            <div class="flex flex-col flex-grow w-full max-w-xl bg-slate-600 shadow-xl rounded-lg overflow-hidden ">
+                <div class="flex w-full mt-2 pl-4 space-x-3 max-w-xs">
+                    <ul class="w-full">
+                        {messages.map((msg, index) => (
+                            <li
+                            class="mb-4 bg-white p-3 rounded-r-lg rounded-bl-lg space-y-2 break-words"
+                            key={index}>{msg}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div class="mt-auto">
+                    <form onSubmit={handleSubmit}>
+                        <div class="bg-white p-4 flex items-center rounded-md">
+                            <input
+                                class="flex-1 border rounded-full px-4 py-2 focus:outline-none"
+                                type="text"
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                required
+                            />
+                            <button type="submit" class="bg-blue-500 text-white rounded-full p-2 ml-2 hover:bg-blue-600 focus:outline-none">Send</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
+        
     );
 };
 
